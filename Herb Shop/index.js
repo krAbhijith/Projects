95c0223b6704d4148489aed8e65d6fcfaefe2db0
Abhijith K R <abhijithkrofficial@gmail.com>
2023-11-02 07:30:26 +0530

    herb shop product added

---
+++ Herb Shop/index.js
@@ -0,0 +1,37 @@
+function login(){
+    document.querySelector('.login-wrapper').style.display="flex"
+}
+function closeLogin(){
+    document.querySelector('.login-wrapper').style.display="none"
+}
+function store(){
+    var username=document.querySelector('.usr').value
+    var password=document.querySelector('.pwd').value
+    if(username=="aami"){    
+    }
+    else {
+        alert("Username is Wrong")
+    }
+    if(password=="nihal"){
+    }
+    else{
+        alert("password is Wrong")
+    }
+}
+function signup(){
+    document.querySelector('.signup-wrapper').style.display="flex"
+}
+function closesignup(){
+    document.querySelector('.signup-wrapper').style.display="none"
+}
+function store2(){
+    var username=document.querySelector('.usr1').value
+    var password=document.querySelector('.pwd1').value
+    var confirmpassword=document.querySelector('.pwd2').value
+    username==""
+        if(password==confirmpassword){
+            alert("pasword is correct")
+          }else{
+            alert("paswword Dont Match ")
+        }
+    }
